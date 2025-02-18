66edd2a5bf90dfff683b83a393f2b6f07942d8d8
Victoria <vicktoria.sin@yandex.ru>
2025-02-18 21:29:32 +0300

    add logic to process student answer

--- hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,8 +3,11 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,10 +24,33 @@ public class TestServiceImpl implements TestService {
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
 
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
-            testResult.applyAnswer(question, isAnswerValid);
+        int questionCount = 0;
+        for (var question : questions) {
+            ioService.printFormattedLine("Question №%d: %s", ++questionCount, question.text());
+            if (question.answers() != null && !question.answers().isEmpty()) {
+                int validAnswerNumber = printAnswersAndDetectRightAnswerNumber(question.answers());
+                testResult.applyAnswer(question, validAnswerNumber == getStudentAnswer(question.answers().size()));
+            }
+            ioService.printLine("");
         }
         return testResult;
+    }
+
+    private int printAnswersAndDetectRightAnswerNumber(List<Answer> answers) {
+        ioService.printFormattedLine("Possible answers:");
+        int answerCount = 0;
+        int validAnswerNumber = -1;
+        for (Answer answer : answers) {
+            ioService.printFormattedLine("%d) %s", ++answerCount, answer.text());
+            if (answer.isCorrect()) {
+                validAnswerNumber = answerCount;
+            }
+        }
+        return validAnswerNumber;
+    }
+
+    private int getStudentAnswer(int answerCount) {
+        String errorMessage = "You must enter the number of one of the answer options";
+        return ioService.readIntForRangeWithPrompt(1, answerCount, "Enter your question: ", errorMessage);
     }
 }
