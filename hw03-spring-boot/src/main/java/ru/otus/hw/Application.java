31122c4cacc61160a458bb7678e7f2077b332c01
Victoria <vicktoria.sin@yandex.ru>
2025-04-07 21:55:02 +0300

    add banner

--- hw03-spring-boot/src/main/java/ru/otus/hw/Application.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import ru.otus.hw.config.AppProperties;
-import ru.otus.hw.service.StreamsIOService;
 
 @EnableConfigurationProperties(AppProperties.class)
 @SpringBootApplication
