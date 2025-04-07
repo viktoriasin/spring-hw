7338290917cf89baed83962a9cd9333b133fa4ff
Victoria <vicktoria.sin@yandex.ru>
2025-04-07 18:47:28 +0300

    move testserviceimpl to localized usage

--- hw03-spring-boot/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,8 +3,11 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,11 +26,31 @@ public class TestServiceImpl implements TestService {
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
 
+        int questionCount = 0;
         for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
-            testResult.applyAnswer(question, isAnswerValid);
+            ioService.printFormattedLineLocalized("TestService.question", ++questionCount, question.text());
+            List<Answer> answers = question.answers();
+            if (answers != null && !answers.isEmpty()) {
+                printAnswers(answers);
+                int studentAnswerNumber = getStudentAnswerNumber(answers.size());
+                testResult.applyAnswer(question, answers.get(studentAnswerNumber - 1).isCorrect());
+            }
+            ioService.printLine("");
         }
         return testResult;
+    }
+
+    private void printAnswers(List<Answer> answers) {
+        ioService.printLineLocalized("TestService.possible.answers");
+        int answerCount = 0;
+        for (Answer answer : answers) {
+            ioService.printFormattedLineLocalized("TestService.possible.answers.text", ++answerCount, answer.text());
+        }
+    }
+
+    private int getStudentAnswerNumber(int answerCount) {
+        return ioService.readIntForRangeWithPromptLocalized(1, answerCount,
+            "TestService.possible.answers.enter", "TestService.possible.error.message");
     }
 
 }
