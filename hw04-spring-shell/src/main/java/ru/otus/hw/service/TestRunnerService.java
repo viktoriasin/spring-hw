58034a349b9eb3f75968784baff49be644571f93
Victoria <vicktoria.sin@yandex.ru>
2025-04-18 21:57:48 +0300

    delete unessessary files

--- hw04-spring-shell/src/main/java/ru/otus/hw/service/TestRunnerService.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.hw.service;
-
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-
-public interface TestRunnerService extends ApplicationRunner {
-    void run(ApplicationArguments applicationArguments);
-}
