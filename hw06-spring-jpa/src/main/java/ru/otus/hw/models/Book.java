128f60d52dceaac37870c1366f0eda8307416448
Victoria <vicktoria.sin@yandex.ru>
2025-04-25 18:36:26 +0300

    add manytoone author mapping

--- hw06-spring-jpa/src/main/java/ru/otus/hw/models/Book.java
+++ hw06-spring-jpa/src/main/java/ru/otus/hw/models/Book.java
@@ -1,5 +1,6 @@
 package ru.otus.hw.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,12 +10,47 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "books")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
 
+    @Column(name = "title")
     private String title;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", foreignKey = @ForeignKey(name = "AUTHOR_ID_FK"))
     private Author author;
 
     private List<Genre> genres;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+            "id=" + id +
+            ", title='" + title + '\'' +
+            '}';
+    }
+
+    @Override
+    public final boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof Book book)) {
+            return false;
+        }
+
+        return id == book.id && title.equals(book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Long.hashCode(id);
+        result = 31 * result + title.hashCode();
+        return result;
+    }
 }
