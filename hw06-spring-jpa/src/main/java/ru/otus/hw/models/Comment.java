8d20ff9f3edac2bca6ccbfcbc1a6ee9a916a03a6
Victoria <vicktoria.sin@yandex.ru>
2025-04-25 19:10:40 +0300

    add comments entity

---
+++ hw06-spring-jpa/src/main/java/ru/otus/hw/models/Comment.java
@@ -0,0 +1,25 @@
+package ru.otus.hw.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "text")
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", foreignKey = @ForeignKey(name = "BOOK_ID_FK"))
+    private Book book;
+}
