70ced68cb4b4606bc266247ab8f5e40d9ea9a5af
Victoria <vicktoria.sin@yandex.ru>
2025-10-01 17:19:16 +0300

    add graph to findbyid repository method

--- hw07-spring-jpa-data/src/main/java/ru/otus/hw/repositories/BookRepository.java
+++ hw07-spring-jpa-data/src/main/java/ru/otus/hw/repositories/BookRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.hw.models.Book;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     @EntityGraph(value = "book-entity-graph", type = EntityGraph.EntityGraphType.LOAD)
     List<Book> findAll();
+
+    @EntityGraph(value = "book-entity-graph", type = EntityGraph.EntityGraphType.LOAD)
+    Optional<Book> findById(Long id);
 }
