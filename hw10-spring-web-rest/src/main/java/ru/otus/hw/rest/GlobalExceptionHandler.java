8ca4cb6a745e46ab3f9d1aa393dbd886468938c3
Victoria <vicktoria.sin@yandex.ru>
2025-12-01 23:10:44 +0300

    fix update controller - add correct display of errors by filed by adding validation exceptioins handler advice

--- hw10-spring-web-rest/src/main/java/ru/otus/hw/rest/GlobalExceptionHandler.java
+++ hw10-spring-web-rest/src/main/java/ru/otus/hw/rest/GlobalExceptionHandler.java
@@ -2,10 +2,15 @@ package ru.otus.hw.rest;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.otus.hw.rest.exceptions.NotFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @ControllerAdvice
@@ -18,6 +23,22 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<String> handeNotFoundException(NotFoundException ex) {
         return ResponseEntity.status(404).body(ERROR_STRING);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(
+        MethodArgumentNotValidException ex) {
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", "Validation failed");
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(err -> {
+            errors.put(err.getField(), err.getDefaultMessage());
+        });
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
 }
